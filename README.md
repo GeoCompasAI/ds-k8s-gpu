651e2d7f6338d252339bd81963af6eed9bd246e6
Rub21 <rub2106@gmail.com>
2024-06-01 00:14:37 -0500

    k8s cluster 1.29

--- README.md
+++ README.md
@@ -1,56 +1,8 @@
-# DevSeed k8s cluster with GPU nodes
+# K8s cluster and nodes creation
 
-This is a lab project aimed at providing easy access to GPU instances for machine learning engineers. The script uses eksctl to deploy a Kubernetes cluster that is equipped with GPU support. In addition, it allows users to choose between Spot and On-Demand instances. Once the cluster is up and running, it also prepares the environment for deploying Helm charts.
-
-e.g: https://github.com/developmentseed/ds-helm-chart
-
-## Create/Delete a cluster
+Config for creation cluster with diferent type of nodes
 
 ```sh
-./deploy.sh <enviroment> <aws_region> create_cluster
-./deploy.sh <enviroment> <aws_region> delete_cluster
-```
-
-Once you have created your cluster, the next step is to create your GPU nodes. The configuration can be found in the instance_list.yaml file, which you can modify according to the instances you require.
-
-## Create/Delete GPU nodes
-
-```sh
-./deploy.sh <enviroment> <aws_region> create_nodes
-./deploy.sh <enviroment> <aws_region> delete_nodes
-```
-
-Once you have created your nodes, you will have a file called `devseed-k8s-<enviroment>-nodes.yaml`, which will contain the available nodegroup_type for you to use to deploy the helm charts.
-
-
-## Using Persistence Disk
-
-If you plan to use persistent disks, make sure to first check the availability zone where your nodes are created. e.g: `us-east-1b` then:
-
-```sh
-aws ec2 create-volume \
-  --region us-east-1 \
-  --availability-zone us-east-1b \
-  --size 20 \
-  --volume-type gp2 \
-  --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=devseed-ebs-test}]'
-```
-
-Copy the `VolumeId` and add in the `values.yaml` file
-
-
-## SSH into the GPU node
-
-You can access the GPU node via SSH using a key pair that has been created in AWS, named `k8s-sam`. If you require access to the associated private key, `k8s-sam.pem`, please reach out to the cluster administrator. If you are setting up your own cluster, you'll also need to create this key pair. To make your experience smoother, essential applications like `git` and `aws-cli` are pre-installed on the node. 
-
-*TODO: install `docker-compose`*
-
-```sh
-sudo systemctl enable docker
-sudo usermod -aG docker ${USER}
-newgrp docker
-ls -la /var/run/docker.sock
-sudo systemctl restart docker
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+./deploy.sh <enviroment> create_cluster
+./deploy.sh <enviroment> delete_cluster
 ```
--- deploy.sh
+++ deploy.sh
@@ -1,81 +1,50 @@
 #!/usr/bin/env bash
 set -e
-
-function get_az_public_subnet() {
-    local vpc_name="$1"
-    local region="$2"
-    local vpc_id=$(aws ec2 describe-vpcs --region "$region" --filters "Name=tag:Name,Values=$vpc_name" --query 'Vpcs[0].VpcId' --output text)
-    local az_public_subnet=$(aws ec2 describe-subnets --region "$region" --filters "Name=vpc-id,Values=$vpc_id" "Name=map-public-ip-on-launch,Values=true" --query 'Subnets[0].AvailabilityZone' --output text)
-    echo "$az_public_subnet"
-}
-
+mkdir -p tmp
 function createNodeGroups {
     ACTION=$1
     read -p "Are you sure you want to $ACTION NODES in the CLUSTER ${CLUSTER_NAME} in REGION ${AWS_REGION}? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
         ######################### GPU NODES #########################
         # Read the YAML file and convert it to a JSON string
-        instance_json=$(python -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=4)' <instance_list.yaml)
-        echo "nodegroup_type" >$CLUSTER_NAME-nodes.yaml
+        instance_json=$(python -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=4)' <instances_list.yaml)
+        echo "nodegroup_type" > ./tmp/$CLUSTER_NAME-nodes.yaml
         # Loop through the JSON array and populate the variables
         length=$(echo $instance_json | jq '.instances | length')
         for ((i = 0; i < $length; i++)); do
             export FAMILY=$(echo $instance_json | jq -r ".instances[$i].family")
-            INSTANCE_TYPE=$(echo $instance_json | jq -r ".instances[$i].instance_type")
             export SPOT_PRICE=$(echo $instance_json | jq -r ".instances[$i].spot_price")
-            nodegroup_type=$(echo $instance_json | jq -r ".instances[$i].nodegroup_type")
-
-            # Split instance_type into spot and ondemand
-            IFS=',' read -ra types <<<"$INSTANCE_TYPE"
-            for type in "${types[@]}"; do
-
-                # Check if instance family exist in the AZ
-                # if aws ec2 describe-instance-type-offerings \
-                #     --location-type availability-zone \
-                #     --filters Name=instance-type,Values=$FAMILY \
-                #     --query "InstanceTypeOfferings[?Location=='$AWS_AVAILABILITY_ZONE'].InstanceType" \
-                #     --output text | grep -q $FAMILY; then
-
-                export NODEGROUP_TYPE=$nodegroup_type-$type
-                echo "##################### $ACTION node: $FAMILY - $type"
-                echo "##########################################"
-                echo "Family:" $FAMILY
-                echo "Spot Price:" $SPOT_PRICE
-                echo "Nodegroup Type:" $NODEGROUP_TYPE
-                echo "- $NODEGROUP_TYPE" >>$CLUSTER_NAME-nodes.yaml
-
-                # Create nodeGroups for the cluster
-                if [ "$ACTION" == "create" ]; then
-                    envsubst <nodeGroups_gpu_$type.yaml | eksctl create nodegroup -f -
-                elif [ "$ACTION" == "delete" ]; then
-                    envsubst <nodeGroups_gpu_$type.yaml | eksctl delete nodegroup --approve -f -
-                fi
-                # else
-                #     echo "Instance type $FAMILY is NOT available in $AWS_AVAILABILITY_ZONE"
-                # fi
-            done
+            export NODEGROUP_TYPE=$(echo $instance_json | jq -r ".instances[$i].nodegroup_type")
+            export DESIRED_CAPACITY=$(echo $instance_json | jq -r ".instances[$i].desired_capacity")
+            export GROUP_NAME_PREFIX="${NODEGROUP_TYPE//_/-}"
+            echo "############## $ACTION node: $FAMILY ##############"
+            echo "###################################################"
+            echo "FAMILY=$FAMILY"
+            echo "SPOT_PRICE=$SPOT_PRICE"
+            echo "NODEGROUP_TYPE=$NODEGROUP_TYPE"
+            echo "GROUP_NAME_PREFIX=$GROUP_NAME_PREFIX"
+            echo "DESIRED_CAPACITY=$DESIRED_CAPACITY"
+            echo "- $NODEGROUP_TYPE" >>./tmp/$CLUSTER_NAME-nodes.yaml
+            # Create nodeGroups for the cluster
+            if [ "$ACTION" == "create" ]; then
+                envsubst <nodeGroups_workers.yaml | eksctl create nodegroup -f -
+            elif [ "$ACTION" == "delete" ]; then
+                envsubst <nodeGroups_workers.yaml | eksctl delete nodegroup --approve -f -
+            fi
         done
-        ######################### CPU NODES #########################
-        if [ "$ACTION" == "create" ]; then
-            envsubst <nodeGroups_cpu_workers.yaml | eksctl create nodegroup -f -
-        elif [ "$ACTION" == "delete" ]; then
-            envsubst <nodeGroups_cpu_workers.yaml | eksctl delete nodegroup --approve -f -
-        fi
     fi
 }
 
 function createCluster {
     read -p "Are you sure you want to CREATE a CLUSTER ${CLUSTER_NAME} in REGION ${AWS_REGION}? (y/n): " confirm
     if [[ $confirm == [Yy] ]]; then
+
         # Create cluster
         envsubst <cluster.yaml | eksctl create cluster -f -
 
         # Create ASG policy
-        envsubst <policy.template.json >policy.json
-        aws iam create-policy --policy-name ${AWS_POLICY_NAME} --policy-document file://policy.json
-
-        # Create CPU node
-        envsubst <nodeGroups_cpu.yaml | eksctl create nodegroup -f -
+        envsubst <policy.template.json >./tmp/policy.json
+        aws iam create-policy --policy-name ${AWS_POLICY_NAME} --policy-document file://tmp/policy.json
 
         # Get cluster credentials
         aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}
@@ -93,21 +62,17 @@ function createCluster {
         envsubst <asg-autodiscover.yaml | kubectl apply -f -
         kubectl get pods --namespace=kube-system | grep autoscaler
 
-        # Install cert-manager
-        helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.9.1 --set installCRDs=true
-        kubectl get pods -n cert-manager
-
-        # Install nginx-ingress
-        helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-        helm repo update
+        # Instaling ingress-nginx and cert-manager
+        helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --set installCRDs=true
         helm install nginx-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+        kubectl get pods -n cert-manager
+        kubectl get pods -n ingress-nginx
 
-        # Update aws-auth
-        kubectl get configmap aws-auth -n kube-system -o yaml >aws-auth.yaml
-        echo "Update manually aws-auth.yaml, use as example mapUsers.yaml"
-        echo "kubectl apply -f aws-auth.yaml"
+        # # Update aws-auth
+        # kubectl get configmap aws-auth -n kube-system -o yaml >aws-auth.yaml
+        # echo "Update manually aws-auth.yaml, use as example mapUsers.yaml"
+        # echo "kubectl apply -f aws-auth.yaml"
     fi
-
 }
 
 function deleteCluster {
@@ -118,25 +83,21 @@ function deleteCluster {
     fi
 }
 
-### Main
+### Set env vars
 export ENVIRONMENT=$1
-export AWS_REGION=$2
-export ACTION=$3
+export ACTION=$2
 export AWS_PARTITION="aws"
-export CLUSTER_NAME="devseed-k8s-${ENVIRONMENT}"
-export KUBERNETES_VERSION="1.27"
+export CLUSTER_NAME="geocompas-k8s-${ENVIRONMENT}"
+export KUBERNETES_VERSION="1.29"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-export AWS_POLICY_NAME=devseed-k8s_${ENVIRONMENT}-${AWS_REGION}
+export AWS_POLICY_NAME=geocompas-k8s_${ENVIRONMENT}-${AWS_REGION}
 export AWS_POLICY_ARN=arn:aws:iam::${AWS_ACCOUNT_ID}:policy/${AWS_POLICY_NAME}
-export VPC_NAME="eksctl-${CLUSTER_NAME}-cluster/VPC"
-# export AWS_AVAILABILITY_ZONE=us-east-1f
-export AWS_AVAILABILITY_ZONE=$(get_az_public_subnet $VPC_NAME $AWS_REGION)
-echo "Make sure you have created a Key pairs, called: k8s-sam-${AWS_REGION}..."
-export KEY_PAIR=k8s-sam-${ENVIRONMENT}
-if [ ! -f ${KEY_PAIR}.pem ]; then
-    aws ec2 create-key-pair --key-name ${KEY_PAIR} --query 'KeyMaterial' --output text > ${KEY_PAIR}.pem
-    chmod 400 ${KEY_PAIR}.pem
-fi
+# NOTE if you are plannign to use existing VPC, configure the yaml file
+## Update values, 
+export AWS_REGION='us-east-1'
+export AWS_AVAILABILITY_ZONE='us-east-1a'
+export VPC_NAME="vpc-0463def14f555a7ed"
+export VPC_CIDR="172.31.0.0/16"
 
 ACTION=${ACTION:-default}
 if [ "$ACTION" == "create_cluster" ]; then
@@ -144,10 +105,8 @@ if [ "$ACTION" == "create_cluster" ]; then
 elif [ "$ACTION" == "delete_cluster" ]; then
     deleteCluster
 elif [ "$ACTION" == "create_nodes" ]; then
-    # Create GPU nodes
     createNodeGroups create
 elif [ "$ACTION" == "delete_nodes" ]; then
-    # Delete GPU nodes
     createNodeGroups delete
 else
     echo "The action is unknown."
